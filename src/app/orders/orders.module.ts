445e37ecdf47fa371bd31a6d7473066bcf878dc0
gegorov <gegorov@yahoo.com>
2020-02-28 01:02:00 +0300

    Add tabs to Orders

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -5,6 +5,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { OrdersRoutingModule } from './orders-routing.module';
 import { OrderListComponent } from './order-list/order-list.component';
 
+import { MatTabsModule } from '@angular/material/tabs';
+
 const routes: Routes = [
   {
     path: '',
@@ -13,7 +15,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [CommonModule, OrdersRoutingModule],
+  imports: [CommonModule, MatTabsModule, OrdersRoutingModule],
   declarations: [OrderListComponent],
 })
 export class OrdersModule {}
