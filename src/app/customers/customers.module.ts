f2ec9aaf5700ded74d6800239011ca2edbd923dd
gegorov <gegorov@yahoo.com>
2020-02-28 01:42:53 +0300

    Add customer card

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -4,19 +4,21 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerCardComponent } from './customer-card/customer-card.component';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 const routes: Routes = [
   {
     path: '',
-    component: CustomerListComponent
-  }
+    component: CustomerListComponent,
+  },
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    CustomersRoutingModule
-  ],
-  declarations: [CustomerListComponent]
+  imports: [CommonModule, CustomersRoutingModule, MatButtonModule, MatCardModule, MatIconModule],
+  declarations: [CustomerListComponent, CustomerCardComponent],
 })
-export class CustomersModule { }
+export class CustomersModule {}
