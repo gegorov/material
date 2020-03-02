125eb9c7688e263c5966c236181d7e3e0329b3a2
gegorov <gegorov@yahoo.com>
2020-03-03 00:24:17 +0300

    add message-new and steppers

--- src/app/messages/messages-routing.module.ts
+++ src/app/messages/messages-routing.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MessageListComponent } from './message-list/message-list.component';
-
+import { MessageNewComponent } from './message-new/message-new.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: MessageListComponent
-  }
+    component: MessageListComponent,
+  },
+  {
+    path: 'new',
+    component: MessageNewComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class MessagesRoutingModule { }
+export class MessagesRoutingModule {}
--- src/app/messages/messages.module.ts
+++ src/app/messages/messages.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatIconModule } from '@angular/material/icon';
 
 import { MessagesRoutingModule } from './messages-routing.module';
 import { MessageListComponent } from './message-list/message-list.component';
+import { MessageNewComponent } from './message-new/message-new.component';
 
 const routes: Routes = [
   {
@@ -13,7 +21,17 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [CommonModule, MessagesRoutingModule],
-  declarations: [MessageListComponent],
+  imports: [
+    CommonModule,
+    MessagesRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatStepperModule,
+  ],
+  declarations: [MessageListComponent, MessageNewComponent],
 })
 export class MessagesModule {}
