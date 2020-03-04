70052e7091d5f33e191cd836a92d439c3b4b449d
gegorov <gegorov@yahoo.com>
2020-03-04 15:59:58 +0300

    add tolltips

--- src/app/messages/messages.module.ts
+++ src/app/messages/messages.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatStepperModule } from '@angular/material/stepper';
-import { MatIconModule } from '@angular/material/icon';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { MessagesRoutingModule } from './messages-routing.module';
 import { MessageListComponent } from './message-list/message-list.component';
@@ -33,6 +34,7 @@ const routes: Routes = [
     MatInputModule,
     MatIconModule,
     MatStepperModule,
+    MatTooltipModule,
   ],
   declarations: [MessageListComponent, MessageNewComponent],
 })
